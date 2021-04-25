46e16984121c90e1a1ecd488117358481be17900
xeusteerapat <xeus085@gmail.com>
2021-04-25 18:45:27 +0700

    [EDIT] update auth0 jwt

--- src/functions/auth/auth0Authorizer.ts
+++ src/functions/auth/auth0Authorizer.ts
@@ -1,19 +1,29 @@
+import * as AWS from 'aws-sdk';
 import {
   APIGatewayTokenAuthorizerEvent,
   APIGatewayAuthorizerResult,
   APIGatewayAuthorizerHandler,
 } from 'aws-lambda';
 import 'source-map-support/register';
+import { verify } from 'jsonwebtoken';
+import { JwtToken } from '../../interfaces/JwtToken';
+
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+const client = new AWS.SecretsManager();
+
+let cachedSecret: string;
 
 export const handler: APIGatewayAuthorizerHandler = async (
   event: APIGatewayTokenAuthorizerEvent
 ): Promise<APIGatewayAuthorizerResult> => {
   try {
-    verifyToken(event.authorizationToken);
+    const decodedToken = await verifyToken(event.authorizationToken);
     console.log('User was authorized');
 
     return {
-      principalId: 'user',
+      principalId: decodedToken.sub,
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -44,7 +54,7 @@ export const handler: APIGatewayAuthorizerHandler = async (
   }
 };
 
-function verifyToken(authHeader: string) {
+async function verifyToken(authHeader: string): Promise<JwtToken> {
   if (!authHeader) {
     throw new Error('No authorization header');
   }
@@ -55,4 +65,23 @@ function verifyToken(authHeader: string) {
 
   const split = authHeader.split(' ');
   const token = split[1];
+
+  const secretObject: any = await getSecret();
+  const secret = secretObject[secretField];
+
+  return verify(token, secret) as JwtToken;
+}
+
+async function getSecret() {
+  if (cachedSecret) return cachedSecret;
+
+  const data = await client
+    .getSecretValue({
+      SecretId: secretId,
+    })
+    .promise();
+
+  cachedSecret = data.SecretString;
+
+  return JSON.parse(cachedSecret);
 }
